f31da1a3fd7492b0b3a248111f9359e1bb887572
Elliot Hesp <elliot.hesp@gmail.com>
2013-10-28 11:30:43 +0000

    added DB config file

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+	if(!defined('ACCESS'))
+		die('Direct access not permitted.');
+	else
+	{
+		$host = 'localhost';
+		$database = 'govspend';
+		$user = 'user';
+		$password = 'password';
+	}
+	
+?>
